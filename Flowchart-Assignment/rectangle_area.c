9927d1af14fe04821146ba54b0a45c15727ba65d
Bhavin S <Bhavin.22508@stu.upes.ac.in>
2025-08-19 03:40:17 +0530

    Create rectangle_area.c

---
+++ Flowchart-Assignment/rectangle_area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+int length = 12;
+int breadth = 16;
+int area;
+area = length * breadth;
+printf("Length = %d\n", length);
+printf("Breadth = %d\n", breadth);
+printf("Area of rectangle = %d\n", area);
+return 0;
+}
