87325a88ed10db0e382021b8185253e1f2ff6995
Bhavin S <Bhavin.22508@stu.upes.ac.in>
2025-08-26 11:11:36 +0530

    Update and rename UPES/Lab3/Q6 to UPES/Lab Experiments/Lab3/Q6.c

--- UPES/Lab Experiments/Lab3/Q6.c
+++ UPES/Lab Experiments/Lab3/Q6.c
@@ -4,8 +4,8 @@ int main() {
     float celsius, fahrenheit;
     printf("Enter temperature in Celsius: ");
     scanf("%f", &celsius);
+    
     fahrenheit = (celsius * 9 / 5) + 32;
     printf("Temperature in Fahrenheit = %.2f\n", fahrenheit);
-
     return 0;
 }
